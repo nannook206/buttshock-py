4217e860c94b6fd5ea66bf40f69ee7dfc0ccb51d
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-08 21:45:19 -0700

    Key retreival working

--- src/erosoutsider.py
+++ src/erosoutsider.py
@@ -5,8 +5,8 @@ class ErosOutsiderError(Exception):
     pass
 
 
-class ErosOutsiderBase():
-    def __init__(self, port):
+class ErosOutsiderBase(object):
+    def __init__(self):
         "docstring"
         self.key = None
 
@@ -16,41 +16,45 @@ class ErosOutsiderBase():
     def _receive_internal(self, length):
         raise RuntimeError("This should be overridden!")
 
-    def _send(self, data):
-        return self._send_internal("".join(map(chr, data)))
+    def _send_check(self, data):
+        checksum = sum(data) % 256
+        data.append(checksum)
+        print data
+        return self._send_internal(data)
 
     def _receive(self, length):
         data = map(ord, self._receive_internal(length))
         if len(data) < length:
             raise ErosOutsiderError("Received unexpected length %d, expected %d!" % (len(data), length))
+        return data
 
     def _receive_check(self, length):
         # decrypt if encrypted
-        data = self._receive_internal(length)
+        data = self._receive(length)
         if self.key:
             pass
         # Test checksum
         checksum = data[-1]
         s = sum(data[:-1]) % 256
         if s != checksum:
-            raise ErosOutsiderError("Checksum mismatch! 0x%02x != 0x%02x" % (s, checksum))
+            raise ErosOutsiderError("Checksum mismatch! 0x%.02x != 0x%.02x" % (s, checksum))
         return data[:-1]
 
     def read(self, address):
-        self._send()
+        pass
 
     def write(self, address, data):
-        self._send()
+        pass
 
     def perform_handshake(self):
         # Handshake
-        for i in range(3):
-            self._send([0x0])
-            check = self._receive(1)
-            if check[0] != 0x7:
-                raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x07!" % (check[0]))
+        for i in range(4):
+            self._send_internal([0x0])
+            check = self._receive(1)[0]
+            if check != 0x7:
+                raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x07!" % (check))
         # Key retreival
-        self._send([0x2f, 0x00])
+        self._send_check([0x2f, 0x29])
         key_info = self._receive_check(3)
         if key_info[0] != 0x21:
                 raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x21!" % (key_info[0]))
@@ -60,9 +64,11 @@ class ErosOutsiderBase():
 class ErosOutsiderSerial(ErosOutsiderBase):
     def __init__(self, port):
         "docstring"
+        super(ErosOutsiderSerial, self).__init__()
         self.port = serial.Serial(port, 19200, timeout=1,
                                   parity=serial.PARITY_NONE,
-                                  bytesize=8, stopbits=1)
+                                  bytesize=8, stopbits=1,
+                                  xonxoff=0, rtscts=0)
 
     def _send_internal(self, data):
         return self.port.write(data)
@@ -70,10 +76,17 @@ class ErosOutsiderSerial(ErosOutsiderBase):
     def _receive_internal(self, length):
         return self.port.read(length)
 
+    def close(self):
+        self.port.close()
+
 def main():
     e = ErosOutsiderSerial("/dev/ttyS0")
-    e.perform_handshake()
-    print e.key
+    try:
+        e.perform_handshake()
+    except ErosOutsiderError, b:
+        e.close()
+        raise b
+    e.close()
 
 if __name__ == "__main__":
     main()
