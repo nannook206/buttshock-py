0fbfc88d0e35f4db81134344100195827022fca8
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-14 22:13:22 -0700

    More screen glitching comments

--- examples/glitch-screen.py
+++ examples/glitch-screen.py
@@ -13,6 +13,10 @@ import erosoutsider
 def main():
     eo = erosoutsider.ErosOutsiderSerialSync("/dev/ttyUSB0")
     eo.perform_handshake()
+    # Write over registers r14-17, though this doesn't seem to crash the ET312.
+    # However, it does anger the box, making it try to print a "Shut off
+    # power!!" message. It doesn't display right away, so instead we end up
+    # with offset string indexes when it shows up when we go to the mode menu.
     eo.write(0x4014, [0x9, 0x4, 0x3, 0x24])
     eo.close()
 
