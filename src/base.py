f09f39da455c99542b1687dd6f8c647df933f98b
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-14 19:00:56 -0700

    Start dividing up modules, add lots of comments

---
+++ src/base.py
@@ -0,0 +1,113 @@
+# ErosOutsider - Base Module
+#
+# Contains base classes for communicating with the to the ErosTek ET-312B
+# Electrostim Unit.
+
+
+class ErosOutsiderError(Exception):
+    """
+    General exception class for ET312 errors
+    """
+    # TODO Should probably add some sort of error codes
+    pass
+
+
+class ErosOutsiderBase(object):
+    """
+    Base class for ET-312 communication. Should be inherited by other classes that
+    implement specific communication types, such as RS-232.
+    """
+    def __init__(self):
+        "Initialization function"
+        # Set the crypto key to None, since it's used to tell whether or not we
+        # should encrypt outgoing messages.
+        self.key = None
+
+    def _send_internal(self, data):
+        """Internal send function, to be implemented by inheritors."""
+        raise ErosOutsiderError("This should be overridden!")
+
+    def _receive_internal(self, length):
+        """Internal receive function, to be implemented by inheritors."""
+        raise ErosOutsiderError("This should be overridden!")
+
+    def _send_check(self, data):
+        """Takes data, calculates checksum, encrypts if key is available."""
+        # Append checksum before encrypting
+        checksum = sum(data) % 256
+        data.append(checksum)
+        # Only encrypt if we have a key
+        if self.key:
+            data = map(lambda x: x ^ self.key, data)
+        return self._send_internal(data)
+
+    def _receive(self, length):
+        """Receive function that handles type conversion and length checks, but does
+        not calculate checksum.
+
+        """
+        data = map(ord, self._receive_internal(length))
+        if len(data) < length:
+            raise ErosOutsiderError("Received unexpected length %d, expected %d!" % (len(data), length))
+        return data
+
+    def _receive_check(self, length):
+        """Receive function that handles type conversion and length checks, plus
+        calculates and checks checksum
+
+        """
+        data = self._receive(length)
+        # Test checksum
+        checksum = data[-1]
+        s = sum(data[:-1]) % 256
+        if s != checksum:
+            raise ErosOutsiderError("Checksum mismatch! 0x%.02x != 0x%.02x" % (s, checksum))
+        return data[:-1]
+
+    def read(self, address):
+        """Read a certain number of bytes from memory at the address given. Address
+        corresponds to the table in the serial protocol documentation.
+
+        """
+        self._send_check([0x3c, address >> 8, address & 0xff])
+        data = self._receive_check(3)
+        return data[1]
+
+    def write(self, address, data):
+        """Writes a certain number of bytes to memory at the address given. Address
+        corresponds to the table in the serial protocol documentation.
+
+        """
+        self._send_check([0x4d, address >> 8, address & 0xff, data])
+        data = self._receive(1)
+        return data[0]
+
+    def perform_handshake(self):
+        """Performs the handshake and key exchange routine expected on box connection.
+
+        Throws exception on connection issues, which can happen frequently with
+        ET312 Firmware Versions 1.6 and below.
+
+        """
+        # TODO throw an exception if we just get a shitload of 0x7. Why does
+        # this box suck so much.
+
+        # Handshake portion
+        for i in range(2):
+            self._send_internal([0x0])
+            check = self._receive(1)[0]
+            if check != 0x7:
+                raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x07!" % (check))
+
+        # Send our chosen key over
+        #
+        # chosen by fair dice roll (oh fuck it no one cares about your xkcd
+        # joke it's just 0)
+        self._send_check([0x2f, 0x00])
+        key_info = self._receive_check(3)
+        if key_info[0] != 0x21:
+            raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x21!" % (key_info[0]))
+
+        # Generate final key here. It's usually 0x55 ^ our_key ^ their_key, but
+        # since our key is 0, we can shorten it to 0x55 ^ their_key
+        self.key = 0x55 ^ key_info[1]
--- src/erosoutsider.py
+++ src/erosoutsider.py
@@ -1,93 +1,28 @@
 import serial
-import time
-
-class ErosOutsiderError(Exception):
-    pass
 
 
-class ErosOutsiderBase(object):
-
-    def __init__(self):
-        "docstring"
-        self.key = None
-
-    def _send_internal(self, data):
-        raise RuntimeError("This should be overridden!")
-
-    def _receive_internal(self, length):
-        raise RuntimeError("This should be overridden!")
-
-    def _send_check(self, data):
-        # Append checksum before encrypting
-        checksum = sum(data) % 256
-        data.append(checksum)
-        # Only encrypt if we have a key
-        if self.key:
-            data = map(lambda x: x ^ self.key, data)
-        return self._send_internal(data)
-
-    def _receive(self, length):
-        data = map(ord, self._receive_internal(length))
-        if len(data) < length:
-            raise ErosOutsiderError("Received unexpected length %d, expected %d!" % (len(data), length))
-        return data
-
-    def _receive_check(self, length):
-        data = self._receive(length)
-        # Test checksum
-        checksum = data[-1]
-        s = sum(data[:-1]) % 256
-        if s != checksum:
-            raise ErosOutsiderError("Checksum mismatch! 0x%.02x != 0x%.02x" % (s, checksum))
-        return data[:-1]
-
-    def read_sync(self, address):
-        self._send_check([0x3c, address >> 8, address & 0xff])
-        data = self._receive_check(3)
-        return data[1]
-
-    def write_sync(self, address, data):
-        self._send_check([0x4d, address >> 8, address & 0xff, data])
-        data = self._receive(1)
-        return data[0]
-
-    def perform_handshake(self):
-        # TODO throw an exception if we just get a shitload of 0x7. Why does
-        # this box suck so much.
-
-        # Handshake portion
-        for i in range(2):
-            self._send_internal([0x0])
-            check = self._receive(1)[0]
-            if check != 0x7:
-                raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x07!" % (check))
-        # Send our chosen key over
-        #
-        # chosen by fair dice roll (oh fuck it no one cares about your xkcd
-        # joke it's just 0)
-        self._send_check([0x2f, 0x00])
-        key_info = self._receive_check(3)
-        if key_info[0] != 0x21:
-            raise ErosOutsiderError("Handshake received 0x%.02x, expected 0x21!" % (key_info[0]))
-
-        # Generate final key here
-        self.key = 0x55 ^ key_info[1]
 
     def glitch_menu(self):
         # Run these 4 commands then press "Menu", then "Up", menu will glitch
         # but still be usable.
 
         # Correct. Do not change
-        # self.write_sync(0x4014, 0x09)
-        # self.write_sync(0x4015, 0x04)
-        # self.write_sync(0x4016, 0x03)
-        # self.write_sync(0x4017, 0x24)
+        self.write_sync(0x4014, 0x09)
+        self.write_sync(0x4015, 0x04)
+        self.write_sync(0x4016, 0x03)
+        self.write_sync(0x4017, 0x24)
 
     def press_up_button(self):
         self.write_sync(0x4069, 0x40)
 
     def press_down_button(self):
         self.write_sync(0x4069, 0x40)
+
+    def dump_ram(self, filename):
+        f = open(filename, "wb")
+        for i in range(0x4000, 0x4400):
+            f.write(chr(self.read_sync(i)))
+        f.close()
 
     def dump_rom(self):
         f = open("ram3.bin", "wb")
@@ -138,12 +73,13 @@ def main():
     e.perform_handshake()
     #e.dump_rom()
     e.glitch_menu()
-    while True:
-        s = ""
-        for i in range(0x4000, 0x4020):
-            s += "0x%.02x " % (e.read_sync(i))
-        print s
-        raw_input()
+    # i = 0
+    # while True:
+    #     raw_input()
+    #     print "Dumping"
+    #     e.dump_ram("ram%d.bin" % (i))
+    #     print "Dumped"
+    #     i += 1
     e.close()
 
 if __name__ == "__main__":
