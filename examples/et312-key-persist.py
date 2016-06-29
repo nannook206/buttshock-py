f69c333fdf6e3db25e4203e136548c080273b484
Kyle Machulis <kyle@nonpolynomial.com>
2016-06-28 22:53:11 -0700

    Continued work on key persistence testing

--- examples/et312-key-persist.py
+++ examples/et312-key-persist.py
@@ -7,15 +7,45 @@ import sys
 sys.path.append("..")
 
 import buttshock
+import argparse
 
 
 def main():
-    et312 = buttshock.ButtshockET312SerialSync("/dev/ttyS0")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", dest="serial_port",
+                        help="Serial Port to use")
+    parser.add_argument("-k", "--key", dest="key",
+                        help="Predetermined key to use")
+    parser.add_argument("-f", "--key_file", dest="key_file",
+                        help="File to store/load box communications key from.")
+
+    args = parser.parse_args()
+
+    if not args.serial_port:
+        print("Serial port argument is required!")
+        sys.exit(1)
+
+    if args.key and not int(args.key):
+        print("Predefined key must be an integer!")
+        sys.exit(1)
+
+    et312 = buttshock.ButtshockET312SerialSync(args.serial_port,
+                                               key=int(args.key) if args.key else None,
+                                               key_file=args.key_file)
+    if not args.key:
+        try:
+            et312.perform_handshake()
+            print("Key is {0:#x} ({0})".format(et312.key, et312.key))
+        except buttshock.ButtshockError as e:
+            print("Handshake failed, trying communication with predetermined key")
+
+    # Get the current mode
     try:
-        et312.perform_handshake()
-        print("Key is {0:#x}".format(et312.key))
-    except buttshock.ButtshockError as e:
-        print(e)
+        mode = et312.read(0x407b)
+        print("Current box mode: {0:#x}".format(mode))
+    except Exception as e:
+        print("Current mode read failed!")
+        raise e
     et312.close()
 
 if __name__ == "__main__":
