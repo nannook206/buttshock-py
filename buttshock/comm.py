058b62154d560ad224b713e7d847d17cbe4c2ef2
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-04 00:09:31 -0700

    Add checks for python 2/3 in serial comms

--- buttshock/comm.py
+++ buttshock/comm.py
@@ -19,7 +19,7 @@ class ButtshockET312SerialSync(ButtshockET312Base):
     """
     def __init__(self, port, key=None, key_file=None):
         """Initialization function. Follows RAII, so creating the object opens the port."""
-        super().__init__(key, key_file)
+        super(ButtshockET312SerialSync, self).__init__(key, key_file)
         self.port = serial.Serial(port, 19200, timeout=1,
                                   parity=serial.PARITY_NONE,
                                   bytesize=8, stopbits=1,
@@ -31,7 +31,15 @@ class ButtshockET312SerialSync(ButtshockET312Base):
 
     def _receive_internal(self, length):
         """Receive data from ET-312 via serial port object."""
-        return self.port.read(length)
+        data = self.port.read(length)
+        # Make sure there's data to check
+        if len(data) == 0:
+            return []
+        # Python 3
+        if type(data[0]) is int:
+            return data
+        # Python 2
+        return [ord(x) for x in data]
 
     def close(self):
         """Close port."""
