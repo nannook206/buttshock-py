cd150063723a25d65d372898b6cc0848223ee994
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-04 22:55:09 -0700

    Simplify init file again after serial change

--- buttshock/__init__.py
+++ buttshock/__init__.py
@@ -1,11 +1,7 @@
 from .base import ButtshockET312Base, ButtshockError
+from .comm import ButtshockET312SerialSync
+from .emulator import ButtshockET312Emulator, ButtshockET312EmulatorSync, ButtshockET312SerialEmulator
 
-try:
-    from .comm import ButtshockET312SerialSync
-    from .emulator import ButtshockET312Emulator, ButtshockET312EmulatorSync, ButtshockET312SerialEmulator
-except:
-    pass
-
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 VERSION = __version__
