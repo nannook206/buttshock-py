1b84fd78d53d615766ce19b00a2e4b0a6dd50bd9
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-04 23:08:52 -0700

    More setup sillyness

--- buttshock/__init__.py
+++ buttshock/__init__.py
@@ -2,6 +2,6 @@ from .base import ButtshockET312Base, ButtshockError
 from .comm import ButtshockET312SerialSync
 from .emulator import ButtshockET312Emulator, ButtshockET312EmulatorSync, ButtshockET312SerialEmulator
 
-__version__ = '0.2.0'
+__version__ = '0.1.1'
 
 VERSION = __version__
