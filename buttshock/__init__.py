346711fde81d775f17ee6976b55a2693b5abcef1
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-04 20:18:48 -0700

    Fix buttshock import for setuptools

--- buttshock/__init__.py
+++ buttshock/__init__.py
@@ -1,6 +1,10 @@
 from .base import ButtshockET312Base, ButtshockError
-from .comm import ButtshockET312SerialSync
 from .emulator import ButtshockET312Emulator, ButtshockET312EmulatorSync, ButtshockET312SerialEmulator
+
+try:
+    from .comm import ButtshockET312SerialSync
+except:
+    pass
 
 __version__ = '0.1.0'
 
