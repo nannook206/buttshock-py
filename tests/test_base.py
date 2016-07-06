6cfda94d122c587aea6b75b5dadd1c0ace82bcb2
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-05 22:52:23 -0700

    Add more tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,19 @@
+import pytest
+from buttshock.errors import ButtshockIOError
 import buttshock.et312
 
 
 def test_emulator():
     with buttshock.et312.ET312EmulatorSync() as et312:
         assert et312.key == (et312.port.emu.box_key ^ 0x55)
+
+
+def test_missing_serial():
+    import serial
+    with pytest.raises(serial.serialutil.SerialException):
+        buttshock.et312.ET312SerialSync("not-a-port")
+
+
+def test_wrong_serial():
+    with pytest.raises(ButtshockIOError):
+        buttshock.et312.ET312SerialSync(1)
