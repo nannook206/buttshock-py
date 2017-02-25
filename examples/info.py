257d7e287a1564ac0fc01cc96268d649bb177563
Team teledildonics <teledildonics@hmamail.com>
2017-02-25 19:22:56 +0000

    Don't bother with handshake for reading stuff if split or random mode give more info about what's going on show how long mode has been running for

--- examples/info.py
+++ examples/info.py
@@ -18,7 +18,7 @@ def main():
     modes = {0x76:"Waves", 0x77:"Stroke", 0x78:"Climb", 0x79:"Combo", 0x7a:"Intense", 0x7b:"Rhythm",
              0x7c:"Audio1",0x7d:"Audio2", 0x7e:"Audio3", 0x80:"Random1", 0x81:"Random2", 0x82:"Toggle",
              0x83:"Orgasm",0x84:"Torment",0x85:"Phase1",0x86:"Phase2",0x87:"Phase3",
-             0x88:"User1",0x89:"User2",0x8a:"User3",0x8b:"User4",0x8c:"User5"}
+             0x88:"User1",0x89:"User2",0x8a:"User3",0x8b:"User4",0x8c:"User5",0:"None", 0x7f:"Split"}
 
     parser = argparse.ArgumentParser()
 
@@ -46,19 +46,32 @@ def main():
         return
 
     try:
-        print ("[+] trying handshake")
-        et312.perform_handshake()
-        print ("[+] handshake ok")
+        print ("[+] connected")
+# no need to do a handshake unless we want to poke
+#        print ("[+] trying handshake")
+#        et312.perform_handshake()
+#        print ("[+] handshake ok")
 
         print("ADC0 (current sense)\t\t: {0:#x}".format(et312.read(0x4060)))
         print("ADC1 (MA knob)\t\t\t: {0:#x}".format(et312.read(0x4061)))
+        print("\tMA scaled value\t\t: {0:#x}".format(et312.read(0x420d)))        
         print("ADC2 (PSU voltage)\t\t: {0:#x}".format(et312.read(0x4062)))
         print("ADC3 (Battery voltage)\t\t: {0:#x}".format(et312.read(0x4063)))
-        print("Battery at boot\t\t\t: {0:.1f}%".format((et312.read(0x4203))*100/256))
+        print("\tBattery at boot\t\t: {0:.1f}%".format((et312.read(0x4203))*100/256))
         print("ADC4 (Level A knob)\t\t: {0:#x}".format(et312.read(0x4064)))
         print("ADC5 (Level B knob)\t\t: {0:#x}".format(et312.read(0x4065)))
-        print("Current Mode\t\t\t: "+modes[et312.read(0x407b)])
-        print("Mode has been running\t\t: {0:#d} seconds".format(int((et312.read(0x4089)+et312.read(0x408a)*256)*1.048)))
+        currentmode =et312.read(0x407b)
+        print("Current Mode\t\t\t: "+modes[currentmode])
+        if (currentmode == 0x7f):
+            print("\tSplit Mode A\t\t: "+modes[et312.read(0x41f5)])
+            print("\tSplit Mode B\t\t: "+modes[et312.read(0x41f6)])
+        if (currentmode == 0x80):
+            print("\tCurrent Random Mode\t: "+modes[et312.read(0x4074)])
+            timeleft = et312.read(0x4075) - et312.read(0x406a)
+            if (timeleft<0):
+                timeleft+=256
+            print("\tTime until change mode\t: {0:#d} seconds ".format(int(timeleft/1.91)))
+        print("\tMode has been running\t: {0:#d} seconds".format(int((et312.read(0x4089)+et312.read(0x408a)*256)*1.048)))
         
     except Exception as e:
         print(e)
